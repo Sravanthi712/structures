ec76d20d63d279ce8fb2f7f6affc06ccc9945b24
Sravanthi712 <96292238+Sravanthi712@users.noreply.github.com>
2022-03-12 13:48:57 +0530

    Add files via upload

---
+++ filesbasics.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+ FILE *fp
+ fp=fopen("c:\DELL","r");
+ if (fp==NULL)
+ {
+ 	printf("File not found\n");
+}
+else
+{
+	printf("File opened sucessfully\n");//outut :file not found
+	
+	/*char ch=fgetc(fp);
+	printf("%c",ch);
+	ch=fget(fp);
+	printf("\n%c",ch);*/
+	char str[100];
+	fgets(str,40,fp);
+	print("%s",str);
+}
+}
